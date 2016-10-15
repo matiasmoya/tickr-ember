e2ebc90904d60a56f22ead09a9a6100fbf134a99
Matt Hinczuk <matiasmoya@gmail.com>
2016-10-15 15:59:36 -0300

    Adds styles to ember using a bootstrap theme

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend(ApplicationRouteMixin, {
   beforeModel() {
     this._super(...arguments);
+  },
+
+  session: service('session'),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+    }
   }
 });
--- app/routes/tickets/index.js
+++ app/routes/tickets/index.js
@@ -6,13 +6,5 @@ const { service } = Ember.inject;
 export default Ember.Route.extend(AuthenticatedUserRouteMixin, {
   model() {
     return this.store.findAll('ticket');
-  },
-
-  session: service('session'),
-
-  actions: {
-    logout() {
-      this.get('session').invalidate();
-    }
   }
 });
